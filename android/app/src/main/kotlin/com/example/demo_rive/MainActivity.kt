c0c1f833058db8bba66f4bf2e2bd356fcd2f121a
Nguyen Phuc Son Tung <ngphsotu@gmail.com>
2022-11-15 13:59:38 +0700

    flutter demo rive

---
+++ android/app/src/main/kotlin/com/example/demo_rive/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.demo_rive
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare_actor.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String correctPassword = 'admin';
+  String animationType = 'idle';
+
+  final passwordController = TextEditingController();
+  final passwordFocusNode = FocusNode();
+  final usernameFocusNode = FocusNode();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 300,
+              width: 300,
+              child: FlareActor(
+                'assets/Teddy.flr',
+                alignment: Alignment.bottomCenter,
+                fit: BoxFit.contain,
+                animation: animationType,
+                callback: (animation) {
+                  setState(() {
+                    animationType = 'idle';
+                  });
+                },
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.pink,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+              ),
+              onPressed: () => signIn(),
+              child: const Text(
+                "Click",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    passwordFocusNode.addListener(() {
+      if (passwordFocusNode.hasFocus) {
+        setState(() {
+          animationType = 'hands_up';
+        });
+      } else {
+        setState(() {
+          animationType = 'hands_down';
+        });
+      }
+    });
+
+    usernameFocusNode.addListener(() {
+      if (usernameFocusNode.hasFocus) {
+        setState(() {
+          animationType = 'test';
+        });
+      } else {
+        setState(() {
+          animationType = 'idle';
+        });
+      }
+    });
+
+    super.initState();
+  }
+
+  void signIn() {
+    if (animationType == 'hands_up') {
+      setState(() {
+        animationType = 'hands_down';
+      });
+    }
+
+    if (passwordController.text.compareTo(correctPassword) == 0) {
+      setState(() {
+        animationType = "success";
+      });
+    } else {
+      setState(() {
+        animationType = "fail";
+      });
+    }
+  }
+}
